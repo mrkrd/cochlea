4a216bd506ee4ac3395a6cdbab9505b2649f1785
Marek Rudnicki <marekrud@gmail.com>
2009-07-12 23:49:44 +0200

    New plot added

---
+++ thresholds/plot_th.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from audiogram_jackson99 import human_audiogram
+from traveling_waves import real_freq_map
+
+def plot_th():
+    th = np.load('th.npz')
+
+    ax = plt.gca()
+
+    ax.semilogx(real_freq_map, th['sumner_th'], label="Sumner")
+    ax.semilogx(real_freq_map, th['holmberg_th'], label="Holmberg")
+
+    # Get only relevant freq range from human_audiogram
+    max_freq = real_freq_map.max() + 2000
+    min_freq = real_freq_map.min()
+    relevant_range = ((human_audiogram['freq'] > min_freq) &
+                      (human_audiogram['freq'] < max_freq))
+
+    ax.semilogx(human_audiogram['freq'][relevant_range],
+                human_audiogram['threshold'][relevant_range],
+                label="Jackson")
+
+    ax.legend()
+
+    plt.savefig("th.eps")
+
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    plot_th()
