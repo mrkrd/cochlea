011b2703d1de17912aa10dd667d1727e86c28fb2
Marek Rudnicki <marekrud@gmail.com>
2009-07-16 20:34:24 +0200

    rasters.py small updates

--- rasters/rasters.py
+++ rasters/rasters.py
@@ -5,19 +5,21 @@ import thorns as th
 import stuff
 
 def rasters():
-    bf = 1000
+    bf = 500
 
     fs = 100000.0               # Hz
     t = np.arange(0, 0.3, 1.0/fs)
     tpad = np.arange(0, 0.1, 1.0/fs)
     stim = np.sin(2 * np.pi * t * bf)
-    stim = stuff.set_dB_SPL(0, stim)
+
+    stim = stuff.set_dB_SPL(50, stim)
+
     stimpad = np.zeros(tpad.shape)
     stim = np.r_[stimpad, stim, stimpad]
 
-    ear = cochlea.Sumner2002(hsr=250, msr=0, lsr=0, freq=bf)
+    ear = cochlea.Sumner2002(hsr=1, msr=0, lsr=0, freq=bf)
 
-    hsr, msr, lsr = ear.run(fs, stim)
+    hsr, msr, lsr = ear.run(fs, stim, times=250)
 
     spikes = hsr['spikes']
 
