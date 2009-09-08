e528ba96cc79a48b811bab2f797721fac27170d4
Marek Rudnicki <marekrud@gmail.com>
2009-09-08 13:02:26 +0200

    CF_MODE before seting freq

--- cochlea/sumner2002_vesicles.py
+++ cochlea/sumner2002_vesicles.py
@@ -1,5 +1,5 @@
 # Author: Marek Rudnicki
-# Time-stamp: <2009-08-28 16:47:37 marek>
+# Time-stamp: <2009-09-08 13:02:06 marek>
 #
 # Description: Model of auditory periphery as described by Sumner et
 # al. (2002) returning vesicles timings instead of ANF spikes.
@@ -108,15 +108,15 @@ class Sumner2002_Vesicles(AuditoryPeriphery):
             self.bm.set_par("CF_MODE", "single")
             self.bm.set_par("SINGLE_CF", freq)
         elif isinstance(freq, tuple):
-            self.bm.set_par("MIN_CF", freq[0])
-            self.bm.set_par("MAX_CF", freq[1])
-            self.bm.set_par("CHANNELS", freq[2])
             if self.animal == 'gp':
                 self.bm.set_par("CF_MODE", "guinea_pig")
             elif self.animal == 'human':
                 self.bm.set_par("CF_MODE", "human")
             else:
                 assert False
+            self.bm.set_par("MIN_CF", freq[0])
+            self.bm.set_par("MAX_CF", freq[1])
+            self.bm.set_par("CHANNELS", freq[2])
 
 
     def run(self, fs, sound, times=1, output_format='spikes'):
