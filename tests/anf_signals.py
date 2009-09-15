9061892f6e13d7e2c3900802a14fec22fb5f5e04
Marek Rudnicki <marekrud@gmail.com>
2009-09-15 20:37:12 +0200

    Output of two models

--- tests/anf_signals.py
+++ tests/anf_signals.py
@@ -1,5 +1,5 @@
 # Author: Marek Rudnicki
-# Time-stamp: <2009-09-11 14:18:55 marek>
+# Time-stamp: <2009-09-15 20:06:40 marek>
 #
 # Description: Plot output bitmaps
 
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import cochlea
+from traveling_waves import real_freq_map
 
 def main():
     fs = 48000
@@ -14,17 +15,27 @@ def main():
     s = np.sin(2 * np.pi * t * 1000)
     s = cochlea.set_dB_SPL(60, s)
 
+    ear = cochlea.Holmberg2008(hsr=100, msr=0, lsr=0)
+    hsrH, msr, lsr = ear.run(fs, s, times=1, output_format='signals')
+
+
+
     ear = cochlea.Sumner2002(hsr=100, msr=0, lsr=0,
-                             freq=(50, 15000, 100),
+                             freq=(real_freq_map.min(),
+                                   real_freq_map.max(),
+                                   100),
                              animal='human')
+    hsrS, msr, lsr = ear.run(fs, s, times=1, output_format='signals')
 
-    hsr, msr, lsr = ear.run(fs, s, times=1, output_format='signals')
 
-    freq_map = ear.get_freq_map()
-    plt.plot(freq_map)
-    plt.show()
 
-    plt.imshow(hsr.T, aspect='auto')
+    fig = plt.gcf()
+    ax = fig.add_subplot(211)
+    ax.set_title("Holmberg 2008")
+    ax.imshow(hsrH.T, aspect='auto')
+    ax = fig.add_subplot(212)
+    ax.set_title("Sumner 2002")
+    ax.imshow(hsrS.T, aspect='auto')
     plt.show()
 
 if __name__ == "__main__":
