0ea622375304808cc75c2f9160c584fcd9b2f6fd
Marek Rudnicki <marekrud@gmail.com>
2009-09-28 16:16:07 +0200

    *** empty log message ***

--- pycat/_pycat.c
+++ pycat/_pycat.c
@@ -3,20 +3,60 @@
 
 #include "catmodel_IHC.h"
 
-static PyMethodDef
-DSAM_Methods[] =
+
+
+static PyObject*
+catmodel_IHC_wrap(PyObject* self, PyObject* args)
 {
-     {"pycat_ihc", catmodel_IHC_wrap, METH_VARARGS, "IHC module."},
+
+     PyObject *signal_arr, *signal_arg;
+     double cf, nrep, binwidth, reptime, cohc, cihc;
+
+     double *px;
+     double totalstim;
+     PyObject *ihcout_arr;
+     npy_intp dims[1];
+     double *ihcout;
+
+
+     if (!PyArg_ParseTuple(args, "Odddddd", \
+     			   &signal_arg, &cf, &nrep, &binwidth, \
+     			   &reptime, &cohc, &cihc))
+     	  return NULL;
+
+     /* Input sound */
+     signal_arr = PyArray_FROM_OTF(signal_arg, NPY_DOUBLE, NPY_IN_ARRAY);
+     px = PyArray_DATA(signal_arr);
+
+     /* Input: totalstim */
+     totalstim = (npy_intp)floor((reptime*1e3)/(binwidth*1e3));
+
+     /* Output: ihcout / ihcout_arr */
+     dims[0] = totalstim;
+     ihcout_arr = PyArray_SimpleNew(1, dims, NPY_DOUBLE);
+     ihcout = PyArray_DATA(ihcout_arr);
+
+     /* Run IHC function */
+     SingleAN(px, cf, nrep, binwidth, totalstim, cohc, cihc, ihcout);
+
+     return ihcout_arr;
+}
+
+
+
+static PyMethodDef
+PyCat_Methods[] =
+{
+     {"ihc", catmodel_IHC_wrap, METH_VARARGS, "IHC module."},
      {NULL, NULL, 0, NULL}
 };
 
 
 
 PyMODINIT_FUNC
-init_dsam(void)
+init_pycat(void)
 {
-     (void)Py_InitModule("_dsam", DSAM_Methods);
+     (void)Py_InitModule("_pycat", PyCat_Methods);
      import_array();
 }
-
 
