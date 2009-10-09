87b77ba239371ca3a533b6d0297d250c18a60d5b
Marek Rudnicki <marekrud@gmail.com>
2009-10-09 15:01:41 +0200

    Carney2009 added

--- cochlea/__init__.py
+++ cochlea/__init__.py
@@ -3,7 +3,7 @@ from holmberg2008 import Holmberg2008
 from sumner2002_vesicles import Sumner2002_Vesicles
 from lopez_poveda2006 import LopezPoveda2006
 from dsam import set_dB_SPL
-
+from pycat import Carney2009
 
 
 # TODO: (re)move main
