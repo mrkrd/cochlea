e7b0e9747175f6e046905fbc961f840530b1a281
Marek Rudnicki <marekrud@gmail.com>
2009-10-09 14:58:04 +0200

    Carney2009 added for rate intensity

--- tests/rate_intensity/rate_intensity.py
+++ tests/rate_intensity/rate_intensity.py
@@ -1,5 +1,5 @@
 # Author: Marek Rudnicki
-# Time-stamp: <2009-09-23 17:10:41 marek>
+# Time-stamp: <2009-10-09 14:57:31 marek>
 #
 # Description: Rate-intensity function
 
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import cochlea
+import thorns as th
 
 def rate_intensity(ear, fs, cf_list, dbspl_list):
 
@@ -16,8 +17,6 @@ def rate_intensity(ear, fs, cf_list, dbspl_list):
 
     tmax = 0.1
     t = np.arange(0, tmax, 1./fs)
-
-    thalf = tmax * 1000 / 2        # ms
 
     hsr_rate = np.zeros( (len(cf_list), len(dbspl_list)) )
     msr_rate = np.zeros( (len(cf_list), len(dbspl_list)) )
@@ -31,22 +30,18 @@ def rate_intensity(ear, fs, cf_list, dbspl_list):
         for dbspl_idx,dbspl in enumerate(dbspl_list):
             s = cochlea.set_dB_SPL(dbspl, s1)
 
+            print cf, dbspl
             hsr, msr, lsr = ear.run(fs, s)
 
             if hsr != None:
                 all_spikes = np.concatenate(tuple(hsr['spikes']))
-                all_spikes = all_spikes[all_spikes > thalf]
-                hsr_rate[cf_idx,dbspl_idx] = len(all_spikes) / tmax / ear.hsr_num * 2
+                hsr_rate[cf_idx,dbspl_idx] = len(all_spikes) / tmax / ear.hsr_num
             if msr != None:
                 all_spikes = np.concatenate(tuple(msr['spikes']))
-                all_spikes = all_spikes[all_spikes > thalf]
-                msr_rate[cf_idx,dbspl_idx] = len(all_spikes) / tmax / ear.msr_num * 2
+                msr_rate[cf_idx,dbspl_idx] = len(all_spikes) / tmax / ear.msr_num
             if lsr != None:
                 all_spikes = np.concatenate(tuple(lsr['spikes']))
-                print len(all_spikes),
-                all_spikes = all_spikes[all_spikes > thalf]
-                print len(all_spikes)
-                lsr_rate[cf_idx,dbspl_idx] = len(all_spikes) / tmax / ear.lsr_num * 2
+                lsr_rate[cf_idx,dbspl_idx] = len(all_spikes) / tmax / ear.lsr_num
 
 
     return hsr_rate, msr_rate, lsr_rate
@@ -56,10 +51,26 @@ def rate_intensity(ear, fs, cf_list, dbspl_list):
 
 def rate_intensity_sumner2002():
 
-    dbspl_list=range(0,80,2)
+    dbspl_list=range(-20,80,5)
     cf_list = [10000]
 
-    ear = cochlea.Sumner2002(hsr=20, msr=20, lsr=20)
+    ear = cochlea.Sumner2002(hsr=10, msr=10, lsr=10)
+
+    hsr_rate, msr_rate, lsr_rate = rate_intensity(ear, 100000,
+                                                  cf_list=cf_list,
+                                                  dbspl_list=dbspl_list)
+    np.save('hsr_rate.npy', hsr_rate)
+    plt.plot(dbspl_list, hsr_rate[0])
+    plt.plot(dbspl_list, msr_rate[0])
+    plt.plot(dbspl_list, lsr_rate[0])
+    plt.show()
+
+def rate_intensity_carney2009():
+
+    dbspl_list=range(-20,80,5)
+    cf_list = [10000]
+
+    ear = cochlea.Carney2009(hsr=10, msr=10, lsr=10)
 
     hsr_rate, msr_rate, lsr_rate = rate_intensity(ear, 100000,
                                                   cf_list=cf_list,
@@ -72,4 +83,4 @@ def rate_intensity_sumner2002():
 
 
 if __name__ == "__main__":
-    rate_intensity_sumner2002()
+    rate_intensity_carney2009()
