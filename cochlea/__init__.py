954cc84a1b6362a8b00077817345fd551528687a
Marek Rudnicki <marekrud@gmail.com>
2009-12-18 19:43:26 +0100

    rate_intensity simplifications

--- cochlea/__init__.py
+++ cochlea/__init__.py
@@ -2,6 +2,6 @@ from sumner2002 import Sumner2002
 from holmberg2008 import Holmberg2008
 from sumner2002_vesicles import Sumner2002_Vesicles
 from lopez_poveda2006 import LopezPoveda2006
-from dsam import set_dB_SPL
-from pycat import Carney2009
+
+from dsam import set_dbspl
 from pycat import Zilany2009
--- tests/rate_intensity/rate_intensity.py
+++ tests/rate_intensity/rate_intensity.py
@@ -1,5 +1,5 @@
 # Author: Marek Rudnicki
-# Time-stamp: <2009-12-17 18:24:45 marek>
+# Time-stamp: <2009-12-18 19:42:52 marek>
 #
 # Description: Rate-intensity function
 
@@ -10,39 +10,37 @@ import matplotlib.pyplot as plt
 
 import cochlea
 import thorns as th
+import thorns.waves as wv
 
-def rate_intensity(ear, fs, cf_list, dbspl_list):
+def rate_intensity(ear, fs, cf, dbspl_list):
 
-    tmax = 0.1
-    t = np.arange(0, tmax, 1/fs)
+    tmax = 100
+    s = wv.generate_ramped_tone(fs, cf,
+                                tone_duration=tmax,
+                                pad_duration=0)
+    ear.set_freq(cf)
 
-    hsr_rate = np.zeros( (len(cf_list), len(dbspl_list)) )
-    msr_rate = np.zeros( (len(cf_list), len(dbspl_list)) )
-    lsr_rate = np.zeros( (len(cf_list), len(dbspl_list)) )
+    hsr_rates = []
+    msr_rates = []
+    lsr_rates = []
 
+    for dbspl in dbspl_list:
+        s = wv.set_dbspl(dbspl, s)
 
-    for cf_idx,cf in enumerate(cf_list):
-        s1 = np.sin(2 * np.pi * t * cf)
-        ear.set_freq(cf)
+        print "dB_SPL:", dbspl
+        hsr, msr, lsr = ear.run(fs, s)
 
-        for dbspl_idx,dbspl in enumerate(dbspl_list):
-            s = cochlea.set_dB_SPL(dbspl, s1)
+        if hsr != None:
+            hsr_rates.append( th.firing_rate( hsr['spikes'],
+                                              tmax, ear._hsr_sum) )
+        if msr != None:
+            msr_rates.append( th.firing_rate( msr['spikes'],
+                                              tmax, ear._msr_sum) )
+        if lsr != None:
+            lsr_rates.append( th.firing_rate( lsr['spikes'],
+                                              tmax, ear._lsr_sum) )
 
-            print cf, dbspl
-            hsr, msr, lsr = ear.run(fs, s)
-
-            if hsr != None:
-                all_spikes = np.concatenate(tuple(hsr['spikes']))
-                hsr_rate[cf_idx,dbspl_idx] = len(all_spikes) / tmax / ear.hsr_num
-            if msr != None:
-                all_spikes = np.concatenate(tuple(msr['spikes']))
-                msr_rate[cf_idx,dbspl_idx] = len(all_spikes) / tmax / ear.msr_num
-            if lsr != None:
-                all_spikes = np.concatenate(tuple(lsr['spikes']))
-                lsr_rate[cf_idx,dbspl_idx] = len(all_spikes) / tmax / ear.lsr_num
-
-
-    return hsr_rate, msr_rate, lsr_rate
+    return hsr_rates, msr_rates, lsr_rates
 
 
 
@@ -50,43 +48,41 @@ def rate_intensity(ear, fs, cf_list, dbspl_list):
 def rate_intensity_sumner2002():
 
     dbspl_list=range(-20,80,5)
-    cf_list = [10000]
+    cf = 10000
 
-    ear = cochlea.Sumner2002(hsr=10, msr=10, lsr=10)
+    ear = cochlea.Sumner2002()
 
     hsr_rate, msr_rate, lsr_rate = rate_intensity(ear, 100000,
                                                   cf_list=cf_list,
                                                   dbspl_list=dbspl_list)
-    np.save('hsr_rate.npy', hsr_rate)
     plt.plot(dbspl_list, hsr_rate[0])
     plt.plot(dbspl_list, msr_rate[0])
     plt.plot(dbspl_list, lsr_rate[0])
     plt.show()
 
+
+
 def rate_intensity_carney2009():
 
-    dbspl_list=range(-20,80,5)
-    cf_list = [1000]
+    dbspl_list = np.arange(-20, 120, 5)
+    cf = 5000
 
-    ear = cochlea.Carney2009(hsr=50, msr=50, lsr=50,
-                             powerlaw_implnt='approx')
+    ear = cochlea.Zilany2009((50, 50, 50), powerlaw_implnt='approx')
 
-    hsr_rate, msr_rate, lsr_rate = rate_intensity(ear, 100000,
-                                                  cf_list=cf_list,
-                                                  dbspl_list=dbspl_list)
-    np.save('hsr_rate.npy', hsr_rate)
+    hsr_rates, msr_rates, lsr_rates = \
+        rate_intensity(ear, fs=100000, cf=cf, dbspl_list=dbspl_list)
 
     fig = plt.gcf()
     ax = fig.add_subplot(111)
 
-    ax.plot(dbspl_list, hsr_rate[0])
-    ax.plot(dbspl_list, msr_rate[0])
-    ax.plot(dbspl_list, lsr_rate[0])
+    ax.plot(dbspl_list, hsr_rates)
+    ax.plot(dbspl_list, msr_rates)
+    ax.plot(dbspl_list, lsr_rates)
 
     ax.set_xlabel("Intensity (dB SPL)")
     ax.set_ylabel("Rate (spikes / sec)")
 
-    fig.savefig('carney2009-rate-intensity.eps')
+    fig.savefig("zilany2009_rate-intensity.eps")
 
     plt.show()
 
