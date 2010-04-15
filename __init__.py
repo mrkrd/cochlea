ad3fec098c758b9d68f247175aa93d476eb3d63c
Marek Rudnicki <marekrud@gmail.com>
2010-04-15 15:14:06 +0200

    __init__ fixes

--- __init__.py
+++ __init__.py
@@ -2,12 +2,12 @@ from dsam import set_dbspl
 
 from sumner2003 import Sumner2003
 from sumner2002_vesicles import Sumner2002_Vesicles
-from lopez_poveda2006 import LopezPoveda2006
+#from lopez_poveda2006 import LopezPoveda2006
 
 try:
-    from holmberg2008 import Holmberg2008
+    from holmberg2007 import Holmberg2007
 except ImportError:
-    print "Holmberg2008 not loaded."
+    print "Holmberg2007 not loaded."
 
 try:
     from pycat import Zilany2009
