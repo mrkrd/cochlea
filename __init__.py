e13234fc6863d9096d83cf3bb15d7a25779fb69b
Marek Rudnicki <marekrud@gmail.com>
2010-06-15 23:16:17 +0200

    Auto: marek@localhost

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ from _pycat import run_spike_generator
 from _pycat import set_dbspl
 
 from zilany2009 import Zilany2009
+from zilany2009 import Zilany2009_Vesicles
+from zilany2009 import Zilany2009_Voltage_Vesicles
 
 
 
