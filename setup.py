e954e3f4b790c8a2a454359a06a0a97a4073acea
Marek Rudnicki <marekrud@gmail.com>
2010-08-02 19:44:44 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+import numpy
+numpy_include = numpy.get_include()
+
+setup(
+    name = "pycat",
+    version = "1.0",
+    author = "Marek Rudnicki",
+    packages = ["pycat"],
+    package_dir = {"pycat": "src/pycat"},
+    ext_package = "pycat",
+    ext_modules = [
+        Extension("_pycat",
+                  ["src/pycat/_pycat.pyx", "src/pycat/catmodel.c",
+                   "src/pycat/complex.c"],
+                  include_dirs=[numpy_include])
+        ],
+    cmdclass = {"build_ext": build_ext}
+)
---
+++ src/pycat/__init__.py
@@ -0,0 +1,11 @@
+from _pycat import run_ihc
+from _pycat import run_synapse
+from _pycat import run_spike_generator
+from _pycat import set_dbspl
+
+from zilany2009 import Zilany2009
+from zilany2009_vesicles import Zilany2009_Vesicles
+from zilany2009_voltage_vesicles import Zilany2009_Voltage_Vesicles
+
+
+
