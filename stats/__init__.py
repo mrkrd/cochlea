712f74277c04009041d3da21572b083ab2db728f
Marek Rudnicki <marekrud@gmail.com>
2010-10-06 16:50:15 +0200

    Auto: marek@black

---
+++ stats/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+__author__ = "Marek Rudnicki"
+
+
+from isointensity import calc_isointensity_curves
+
--- stats/isointensity.py
+++ stats/isointensity.py
@@ -11,14 +11,13 @@ import multiprocessing
 import thorns.waves as wv
 import thorns as th
 
-def _run_model( (model, cf, freq, dbspl) ):
+def _run_model( (model, cf, fs, freq, dbspl, kwargs) ):
     print os.getpid(), freq, dbspl
 
     tmax = 100
-    fs = 48000
     onset = 10
 
-    ear = model((1500, 0, 0), cf=cf)
+    ear = model((1500, 0, 0), cf=cf, **kwargs)
     # ear = model((250, 0, 0), cf=cf,
     #             powerlaw_implnt='approx',
     #             with_ffGn=False)
@@ -33,43 +32,51 @@ def _run_model( (model, cf, freq, dbspl) ):
     hsr = th.trim(hsr, onset)
     rate_hsr = th.calc_rate(hsr, stimulus_duration=(tmax-onset))
 
-    return rate_hsr
+    return freq, dbspl, rate_hsr
 
 
-def calc_rate_curves(model, cf):
-    space = [(model, cf, freq, dbspl)
+def calc_isointensity_curves(model,
+                             cf=3000,
+                             fs=100e3,
+                             freqs=np.logspace(np.log10(1000), np.log10(8000), 32),
+                             dbspls=np.arange(0, 110, 10),
+                             **kwargs):
+
+    space = [(model, cf, fs, freq, dbspl, kwargs)
              for m in [model]
              for cf in [cf]
-             for freq in np.logspace(np.log10(5000), np.log10(6000), 32)
-             for dbspl in np.arange(0, 90, 20)]
+             for fs in [fs]
+             for freq in freqs
+             for dbspl in dbspls
+             for kwargs in [kwargs]]
 
     pool = multiprocessing.Pool()
     rates = pool.map(_run_model, space)
 
-    freqs = [el[2] for el in space]
-    dbspls = [el[3] for el in space]
-    rates = zip(freqs,dbspls,rates)
     rates = np.rec.array(rates, names='freq,dbspl,rate')
 
     return rates
 
 
 def main():
-    # import pycat
-    # model = pycat.Zilany2009
+    import pycat
+    model = pycat.Zilany2009
+    pars = {'powerlaw_implnt':'approx',
+            'with_ffGn':False}
 
     # import cochlea
     # model = cochlea.Sumner2003
 
-    import cochlea
-    model = cochlea.Holmberg2007
-    import traveling_waves
-    cf = traveling_waves.real_freq_map[75]
+    # import cochlea
+    # model = cochlea.Holmberg2007
 
-    # print _run_model( (model, 5000, 3000, 50) )
+    import traveling_waves as tw
+    cf = tw.real_freq_map[75]
+
+    # print _run_model( (model, cf, 48000, 3000, 50, {}) )
     # exit()
 
-    rates = calc_rate_curves(model, cf)
+    rates = calc_isointensity_curves(model, cf, fs=100e3, **pars)
     print rates
 
     import biggles
