69f1a03fdbf7cfab9d31e19810fffc76008f1f26
Marek Rudnicki <marekrud@gmail.com>
2011-08-02 13:32:02 +0200

    auto: marek@black

--- src/pycat/__init__.py
+++ src/pycat/__init__.py
@@ -8,5 +8,8 @@ from zilany2009 import Zilany2009
 from zilany2009_vesicles import Zilany2009_Vesicles
 from zilany2009_voltage_vesicles import Zilany2009_Voltage_Vesicles
 
-from zilany2009_human_holmberg import Zilany2009_Human_Holmberg
+try:
+    from zilany2009_human_holmberg import Zilany2009_Human_Holmberg
+except ImportError:
+    print "Warning: no human model imported, traveling_waves module needed"
 
