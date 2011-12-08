5d1ad82e97f1e0fafbd8b7f078c8857c7dd50f56
Marek Rudnicki <marekrud@gmail.com>
2011-12-08 16:59:16 +0100

    auto: marek@orange

--- stats/rate_intensity.py
+++ stats/rate_intensity.py
@@ -9,6 +9,7 @@ import multiprocessing
 import os
 
 import thorns as th
+import thorns.stats as ths
 import thorns.waves as wv
 
 def _run_model( (model, dbspl) ):
@@ -25,19 +26,19 @@ def _run_model( (model, dbspl) ):
                                 tone_duration=tmax,
                                 pad_duration=0,
                                 dbspl=dbspl)
-    anf = ear.run(fs, s)
+    anf = ear.run(s, fs)
 
-    hsr = anf[anf['typ']=='hsr']
-    hsr = th.trim(hsr, onset)
-    rate_hsr = th.calc_rate(hsr, stimulus_duration=(tmax-onset))
+    hsr = anf[anf['type']=='hsr']
+    hsr = th.trim(hsr, (onset, None))
+    rate_hsr = ths.rate(hsr)
 
-    msr = anf[anf['typ']=='msr']
-    msr = th.trim(msr, onset)
-    rate_msr = th.calc_rate(msr, stimulus_duration=(tmax-onset))
+    msr = anf[anf['type']=='msr']
+    msr = th.trim(msr, (onset, None))
+    rate_msr = ths.rate(msr)
 
-    lsr = anf[anf['typ']=='lsr']
-    lsr = th.trim(lsr, onset)
-    rate_lsr = th.calc_rate(lsr, stimulus_duration=(tmax-onset))
+    lsr = anf[anf['type']=='lsr']
+    lsr = th.trim(lsr, (onset, None))
+    rate_lsr = ths.rate(lsr)
 
     return rate_hsr, rate_msr, rate_lsr
 
@@ -52,6 +53,8 @@ def calc_rate_intensity(model):
     pool = multiprocessing.Pool()
     rates = pool.map(_run_model, space)
 
+    # rates = map(_run_model, space)
+
     rates = [(dbspl,)+rate for dbspl,rate in zip(dbspls,rates)]
     rates = np.rec.array(rates, names='dbspl,hsr,msr,lsr')
 
@@ -60,9 +63,9 @@ def calc_rate_intensity(model):
 
 def main():
     import biggles
-    import cochlea
+    import pycat
 
-    rates = calc_rate_intensity(cochlea.Sumner2003)
+    rates = calc_rate_intensity(pycat.Zilany2009)
 
     plot = biggles.FramedPlot()
     plot.add( biggles.Curve(rates['dbspl'], rates['hsr']) )
