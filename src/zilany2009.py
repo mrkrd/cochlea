0f1ca19a228010d88f14196457ae77e323bfb19d
Marek Rudnicki <marekrud@gmail.com>
2012-02-17 13:46:09 +0100

    auto: marek@jade

--- src/zilany2009.py
+++ src/zilany2009.py
@@ -21,7 +21,7 @@ class Zilany2009(object):
     name = 'Zilany2009'
 
     def __init__(self, anf_num=(1,1,1), cf=1000,
-                 powerlaw_implnt='approx', with_ffGn=False)
+                 powerlaw_implnt='approx', with_ffGn=False):
         """ Auditory periphery model of a cat (Zilany et al. 2009)
 
         anf_num: (hsr_num, msr_num, lsr_num)
