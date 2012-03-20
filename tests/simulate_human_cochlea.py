c6a07a6e0a38e7c54821b0319c9f2fe13b011e2f
Marek Rudnicki <marekrud@gmail.com>
2012-03-20 15:15:17 -0300

    auto: marek@orange

--- tests/simulate_human_cochlea.py
+++ tests/simulate_human_cochlea.py
@@ -11,28 +11,33 @@ import matplotlib.pyplot as plt
 import thorns as th
 import thorns.waves as wv
 
-import pycat
+import cochlea
 
 def main():
 
-    fs = 100000
+    fs = 100e3
 
-    ear = pycat.Zilany2009_Human(anf_num=(100,0,0),
-                                 cf=(80, 16000, 100))
+    ear = cochlea.Zilany2009_Human(
+        anf_num=(100,0,0),
+        cf=(80, 16000, 100)
+    )
 
-    s = wv.generate_ramped_tone(fs,
-                                freq=600,
-                                tone_duration=50,
-                                ramp_duration=2.5,
-                                pad_duration=20,
-                                dbspl=50)
+    s = wv.generate_ramped_tone(
+        fs,
+        freq=600,
+        tone_duration=50e-3,
+        ramp_duration=2.5e-3,
+        pad_duration=20e-3,
+        dbspl=60
+    )
+    s = np.concatenate( (s, np.zeros(50e-3 * fs)) )
 
-    single_trains = ear.run(s, fs)
+    single_trains = ear.run(s, fs, seed=0)
 
-    anf = th.accumulate(single_trains,
-                        ignore=['anf_idx'])
+    anf = th.accumulate(single_trains, ignore=['index'])
 
     anf_matrix = th.trains_to_signal(anf, fs)
+
     plt.imshow(anf_matrix.T, aspect='auto')
     plt.show()
 
