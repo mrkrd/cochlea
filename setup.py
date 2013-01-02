10c12d96189fde9c7d96f3994cebb95371052f8e
Marek Rudnicki <marekrud@gmail.com>
2013-01-02 14:10:10 +0100

    marek@vanilla

--- setup.py
+++ setup.py
@@ -1,34 +1,24 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
-import numpy
-numpy_include = numpy.get_include()
 
 setup(
     name = "cochlea",
-    version = "3",
+    version = "4",
+    description = "Collection of inner ear models",
     author = "Marek Rudnicki",
+    author_email = "marek.rudnicki@tum.de",
     packages = [
         "cochlea",
         "cochlea.stats"
     ],
-    package_dir = {
-        "cochlea": "src",
-        "cochlea.stats": "src/stats"
+    package_data = {
+        "cochlea": ["data/*.txt", "pars/*.par"]
     },
-    package_data = {"cochlea": ["data/*.txt", "pars/*.par"]},
     ext_package = "cochlea",
-    ext_modules = [
-        Extension("_pycat",
-                  ["src/_pycat.pyx", "src/catmodel.c",
-                   "src/complex.c"],
-                  include_dirs=[numpy_include]),
-        Extension("_tw",
-                  ["src/traveling_waves/_tw.pyx"],
-                  include_dirs=[numpy_include])
-        ],
-    cmdclass = {"build_ext": build_ext}
+    ext_modules = cythonize(
+        ["cochlea/_pycat.pyx", "cochlea/traveling_waves/_tw.pyx"]
+    )
 )
