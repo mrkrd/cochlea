dfff3e797da83a9ea9b3f7cbdc98b87862e3b8af
Marek Rudnicki <marekrud@gmail.com>
2014-05-23 16:35:16 +0200

    generating data for Zilany 2014 tests

--- tests/data_zilany2014/generate_data_zilany2014.m
+++ tests/data_zilany2014/generate_data_zilany2014.m
@@ -1,37 +1,43 @@
 clear all;
-CF    = 1.0e3; % CF in Hz;
-cohc  = 1.0;   % normal ohc function
-cihc  = 1.0;   % normal ihc function
-species = 1;    % 1 for cat (2 for human with Shera et al. tuning; 3 for human with Glasberg & Moore tuning)
-fiberType = 3; % spontaneous rate (in spikes/s) of the fiber BEFORE refractory effects; "1" = Low; "2" = Medium; "3" = High
-implnt = 0;    % "0" for approximate or "1" for actual implementation of the power-law functions in the Synapse
 
-% stimulus parameters
-F0 = CF;     % stimulus frequency in Hz
-Fs = 100e3;  % sampling rate in Hz (must be 100, 200 or 500 kHz)
-T  = 50e-3;  % stimulus duration in seconds
 
-% PSTH parameters
-nrep = 1;              % number of stimulus repetitions (e.g., 50);
+%%% Model parameters
+cf = 1e3;
+cohc = 1.0;
+cihc = 1.0;
+species = 1; % 1: cat; 2: human with Shera et al. tuning; 3: for human with Glasberg & Moore tuning
+noise_type = 0;
+fiber_type = 3; % 3: HSR
+implnt = 0; % 0: approximate; 1: actual
+nrep = 1;
 
-sound = zeros(1, Fs*T);
-sound(10e-3*Fs) = 1;
 
+%%% Stimulus
+fs = 100e3;
+tmax = 50e-3;
+
+sound = zeros(1, fs*tmax);
+sound(10e-3*fs) = 1;
+
+
+%%% Run simmulation
+vihc = model_IHC(sound, cf, nrep, 1/fs, tmax, cohc, cihc, species);
+[meanrate,varrate,psth] = model_Synapse(vihc, cf, nrep, 1/fs, fiber_type, noise_type, implnt);
+
+
+%%% Save results
+save('data_zilany2014.mat', 'fs', 'sound', 'vihc', 'meanrate')
+
+
+%%% Plots
 subplot(4,1,1)
 plot(sound)
-
-vihc = model_IHC(sound, CF, nrep, 1/Fs, T, cohc, cihc, species);
-
 
 subplot(4,1,2)
 plot(vihc)
 
-
-
-[meanrate,varrate,psth] = model_Synapse(vihc, CF, nrep, 1/Fs, fiberType, implnt);
-
 subplot(4,1,3)
-plot(synout)
+plot(meanrate)
 
 subplot(4,1,4)
 plot(psth)
