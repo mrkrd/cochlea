354843da34e5dff223980a6d688088b17d711f12
Marek Rudnicki <marekrud@gmail.com>
2014-10-21 19:50:05 +0200

    brian interop (work in progress)

--- examples/brian_interop.py
+++ examples/brian_interop.py
@@ -3,9 +3,7 @@
 """Interoperability with Brian.
 
 """
-
 from __future__ import division, print_function, absolute_import
-from __future__ import unicode_literals
 
 __author__ = "Marek Rudnicki"
 __copyright__ = "Copyright 2014, Marek Rudnicki"
@@ -18,15 +16,19 @@ import cochlea
 import thorns as th
 import thorns.waves as wv
 
+import brian
+from brian import mV, second, ms, volt
+
 def main():
 
     fs = 100e3
     cf = 1e3
+    tmax = 50e-3
 
     sound = wv.ramped_tone(
         fs=fs,
         freq=cf,
-        duration=50e-3,
+        duration=tmax,
         pad=30e-3,
         dbspl=50,
     )
@@ -43,6 +45,39 @@ def main():
     anfs = cochlea.make_brian_group(anf_trains)
 
     print(anfs)
+
+
+    brainstem = make_brainstem_group(100)
+
+    print(brainstem)
+
+    monitor = brian.SpikeMonitor(brainstem)
+
+
+    net = brian.Network([brainstem, monitor])
+    net.run(tmax*second)
+
+
+    brian.raster_plot(monitor)
+    brian.show()
+
+
+
+def make_brainstem_group(num):
+    """Generate integrate-and-fire neuron group that can be connected to a
+    auditory nerve fibers group.
+
+    """
+    eqs = '''
+    dv/dt = (ge-(v+49*mV))/(2*ms) : volt
+    dge/dt = -ge/(0.5*ms) : volt
+    '''
+
+    group = brian.NeuronGroup(num, eqs, threshold=-50*mV, reset=-60*mV)
+    group.v = -60*mV
+
+    return group
+
 
 
 if __name__ == "__main__":
