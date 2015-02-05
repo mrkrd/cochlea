f98a5bb27532f23d76537f60f73511d76ef05208
Marek Rudnicki <marekrud@gmail.com>
2015-02-05 18:58:39 +0100

    new copyright year

--- doc/conf.py
+++ doc/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'cochlea'
-copyright = u'2014, Marek Rudnicki'
+copyright = u'2014-2015, Marek Rudnicki'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
