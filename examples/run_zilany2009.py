df4bdacc3b36b3a7d837cd985ef8776753bac914
mr <>
2016-07-14 13:58:30 +0200

    update examples

--- examples/run_zilany2009.py
+++ examples/run_zilany2009.py
@@ -1,32 +1,9 @@
 #!/usr/bin/env python
 """
-Copyright 2009-2014 Marek Rudnicki
-
-This file is part of cochlea.
-
-cochlea is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-cochlea is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with cochlea.  If not, see <http://www.gnu.org/licenses/>.
-
-
-Description
------------
-
 Run inner ear model from [Zilany2009]_.
 
 """
-from __future__ import division
-
-__author__ = "Marek Rudnicki"
+from __future__ import division, absolute_import, print_function
 
 
 import numpy as np
@@ -34,22 +11,19 @@ import matplotlib.pyplot as plt
 import scipy.signal as dsp
 
 import thorns as th
-
 import cochlea
 
 def main():
 
     fs = 100e3
 
-    ### Make sound
+    # Make sound
     t = np.arange(0, 0.1, 1/fs)
     s = dsp.chirp(t, 80, t[-1], 20000)
     s = cochlea.set_dbspl(s, 50)
-    s = np.concatenate( (s, np.zeros(10e-3 * fs)) )
+    s = np.concatenate((s, np.zeros(int(10e-3*fs))))
 
-
-
-    ### Run model
+    # Run model
     anf = cochlea.run_zilany2009(
         s,
         fs,
@@ -59,11 +33,9 @@ def main():
         powerlaw='approximate'
     )
 
-
-
-    ### Plot auditory nerve response
+    # Plot auditory nerve response
     anf_acc = th.accumulate(anf, keep=['cf', 'duration'])
-    anf_acc.sort('cf', ascending=False, inplace=True)
+    anf_acc.sort_values('cf', ascending=False, inplace=True)
 
     cfs = anf.cf.unique()
 
--- examples/run_zilany2014.py
+++ examples/run_zilany2014.py
@@ -1,53 +1,28 @@
 #!/usr/bin/env python
-
-# Copyright 2009-2014 Marek Rudnicki
-#
-# This file is part of cochlea.
-#
-# cochlea is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# cochlea is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with cochlea.  If not, see <http://www.gnu.org/licenses/>.
-
-
 """Run inner ear model from [Zilany2014]_.
 
 """
 from __future__ import division, print_function, absolute_import
-
-__author__ = "Marek Rudnicki"
-
 
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as dsp
 
 import thorns as th
-
 import cochlea
 
 def main():
 
     fs = 100e3
 
-    ### Make sound
+    # Make sound
     t = np.arange(0, 0.1, 1/fs)
     s = dsp.chirp(t, 80, t[-1], 20000)
     s = cochlea.set_dbspl(s, 50)
-    pad = np.zeros(10e-3 * fs)
+    pad = np.zeros(int(10e-3 * fs))
     sound = np.concatenate( (s, pad) )
 
-
-
-    ### Run model
+    # Run model
     anf = cochlea.run_zilany2014(
         sound,
         fs,
@@ -58,14 +33,11 @@ def main():
         species='human',
     )
 
-
-    ### Accumulate spike trains
+    # Accumulate spike trains
     anf_acc = th.accumulate(anf, keep=['cf', 'duration'])
-    anf_acc.sort('cf', ascending=False, inplace=True)
+    anf_acc.sort_values('cf', ascending=False, inplace=True)
 
-
-
-    ### Plot auditory nerve response
+    # Plot auditory nerve response
     fig, ax = plt.subplots(2,1)
     th.plot_signal(
         signal=sound,
