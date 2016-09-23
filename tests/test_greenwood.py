c6e3cd0ad87062eb38eaeb58a5424416777d1cb3
Jörg Encke <joerg.encke@tum.de>
2016-09-23 14:13:22 +0200

    Adds Tests for the greenwood functions

---
+++ tests/test_greenwood.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_equal
+import cochlea
+
+def test_invertable():
+    """ Test if the greenwood function is invertable
+
+    """
+    freq = np.linspace(20, 20e3, 100)
+    x = cochlea.greenwood_inverse(freq, 'human')
+    freq_greenwood = cochlea.greenwood(x, 'human')
+
+    assert_almost_equal(freq, freq_greenwood)
+
+def test_function():
+    """Compare function results with correct value
+
+    """
+    freq_0 = cochlea.greenwood(0, 'human')
+
+    freq_0_calc = 165.4 * (1 - 0.88)
+
+    assert(freq_0 == freq_0_calc)
+
+def test_parameters():
+    """ Test if providing parameters results in the same as providing species
+
+    """
+    x = np.linspace(0, 35e-3, 100)
+
+    freq = cochlea.greenwood(x, A=165.4, a=60, k=0.88, species=None)
+    freq_human = cochlea.greenwood(x, 'human')
+
+    x = cochlea.greenwood_inverse(freq, A=165.4, a=60, k=0.88, species=None)
+    x_human = cochlea.greenwood_inverse(freq, 'human')
+
+    assert_equal(freq, freq_human)
+    assert_equal(x, x_human)
