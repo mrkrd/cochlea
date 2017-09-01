4e3f51369b28cbe8642592a05854c0f97316e776
mr <>
2017-09-01 15:36:53 +0200

    zilany2014: ffGn should be calculated for every spike train

--- cochlea/zilany2014/__init__.py
+++ cochlea/zilany2014/__init__.py
@@ -157,7 +157,7 @@ def _run_channel(args):
     trains = []
     for anf_type in anf_types:
 
-        if anf_type not in synout:
+        if (anf_type not in synout) or ffGn:
             ### Run synapse
             synout[anf_type] = _zilany2014.run_synapse(
                 fs=fs,
